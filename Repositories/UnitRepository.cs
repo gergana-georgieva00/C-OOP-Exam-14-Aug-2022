a9206346921366028faf8e01df0a19de30ad22cc
Gergana Georgieva <gerganagergana1@gmail.com>
2022-11-28 00:57:59 +0200

    C# OOP Exam – 14 Aug 2022

---
+++ Repositories/UnitRepository.cs
@@ -0,0 +1,35 @@
+﻿using PlanetWars.Models.MilitaryUnits.Contracts;
+using PlanetWars.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlanetWars.Repositories
+{
+    public class UnitRepository : IRepository<IMilitaryUnit>
+    {
+        private List<IMilitaryUnit> models;
+
+        public UnitRepository()
+        {
+            this.models = new List<IMilitaryUnit>();
+        }
+
+        public IReadOnlyCollection<IMilitaryUnit> Models => this.models.AsReadOnly();
+
+        public void AddItem(IMilitaryUnit unit)
+        {
+            this.models.Add(unit);
+        }
+
+        public IMilitaryUnit FindByName(string inputTypeName)
+        {
+            return this.models.Find(u => u.GetType().Name == inputTypeName);
+        }
+
+        public bool RemoveItem(string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
