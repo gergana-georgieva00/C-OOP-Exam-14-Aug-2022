0a88c071a4ddce52a6fefa5e037153809053592a
Gergana Georgieva <gerganagergana1@gmail.com>
2022-11-27 22:34:58 +0200

    C# OOP Exam – 14 Aug 2022

--- Models/Weapons/Weapon.cs
+++ Models/Weapons/Weapon.cs
@@ -7,6 +7,24 @@ namespace PlanetWars.Models.Weapons
 {
     public abstract class Weapon : IWeapon 
     {
+        private int destructionLevel;
+
+        public Weapon(double price, int destructionLevel)
+        {
+            this.Price = price;
+
+            if (destructionLevel < 1)
+            {
+                throw new ArgumentException("Destruction level cannot be zero or negative.");
+            }
+            else if (destructionLevel > 10)
+            {
+                throw new ArgumentException("Destruction level cannot exceed 10 power points.");
+            }
+
+            this.DestructionLevel = destructionLevel;
+        }
+
         public abstract double Price { get; set; }
 
         public abstract int DestructionLevel { get; set; }
