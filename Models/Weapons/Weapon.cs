28766f0684e1fdf89d2e0be2c090af6b3d490dad
Gergana Georgieva <gerganagergana1@gmail.com>
2022-11-27 20:49:18 +0200

    C# OOP Exam – 14 Aug 2022

---
+++ Models/Weapons/Weapon.cs
@@ -0,0 +1,14 @@
+﻿using PlanetWars.Models.Weapons.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlanetWars.Models.Weapons
+{
+    public abstract class Weapon : IWeapon 
+    {
+        public abstract double Price { get; set; }
+
+        public abstract int DestructionLevel { get; set; }
+    }
+}
