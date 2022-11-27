6ef52ce0a705a40dd53dbcb661ab1af10f7ad00e
Gergana Georgieva <gerganagergana1@gmail.com>
2022-11-27 22:56:18 +0200

    C# OOP Exam – 14 Aug 2022

---
+++ Models/MilitaryUnits/MilitaryUnit.cs
@@ -0,0 +1,25 @@
+﻿using PlanetWars.Models.MilitaryUnits.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlanetWars.Models.MilitaryUnits
+{
+    public abstract class MilitaryUnit : IMilitaryUnit
+    {
+        public MilitaryUnit(double cost)
+        {
+            this.Cost = cost;
+            this.EnduranceLevel = 1;
+        }
+
+        public double Cost { get; }
+
+        public int EnduranceLevel { get; }
+
+        public void IncreaseEndurance()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
