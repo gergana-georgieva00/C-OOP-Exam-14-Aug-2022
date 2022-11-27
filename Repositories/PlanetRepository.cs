a428e6757307a8cd8099a53723706da405ddd547
Gergana Georgieva <gerganagergana1@gmail.com>
2022-11-28 01:01:47 +0200

    C# OOP Exam – 14 Aug 2022

---
+++ Repositories/PlanetRepository.cs
@@ -0,0 +1,35 @@
+﻿using PlanetWars.Models.Planets.Contracts;
+using PlanetWars.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlanetWars.Repositories
+{
+    public class PlanetRepository : IRepository<IPlanet>
+    {
+        private List<IPlanet> planets;
+
+        public PlanetRepository()
+        {
+            this.planets = new List<IPlanet>();
+        }
+
+        public IReadOnlyCollection<IPlanet> Models => this.planets.AsReadOnly();
+
+        public void AddItem(IPlanet model)
+        {
+            this.planets.Add(planet);
+        }
+
+        public IPlanet FindByName(string name)
+        {
+            return this.planets.Find(p => p.Name == name);
+        }
+
+        public bool RemoveItem(string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
