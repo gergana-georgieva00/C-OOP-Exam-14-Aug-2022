e09997c5c4d4281079bbb252a952c352b373b50d
Gergana Georgieva <gerganagergana1@gmail.com>
2022-11-28 13:24:50 +0200

    C# OOP Exam – 14 Aug 2022

--- Models/Planets/Planet.cs
+++ Models/Planets/Planet.cs
@@ -1,16 +1,18 @@
 ﻿using PlanetWars.Models.MilitaryUnits.Contracts;
 using PlanetWars.Models.Planets.Contracts;
 using PlanetWars.Models.Weapons.Contracts;
+using PlanetWars.Repositories;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace PlanetWars.Models.Planets
 {
     public class Planet : IPlanet
     {
-        //private UnitRepository units;
-        //private WeaponRepository weapons;
+        private UnitRepository units;
+        private WeaponRepository weapons;
 
         public Planet(string name, double budget)
         {
@@ -39,6 +41,24 @@ namespace PlanetWars.Models.Planets
         private double CalculateMilitaryPower()
         {
             double totalAmount = 0;
+
+            foreach (var unit in units.Models)
+            {
+                totalAmount += unit.EnduranceLevel;
+            }
+            foreach (var weapon in weapons.Models)
+            {
+                totalAmount += weapon.DestructionLevel;
+            }
+
+            if (this.units.Models.Any(u => u.GetType().Name == "AnonymousImpactUnit"))
+            {
+                totalAmount += totalAmount * 0.3;
+            }
+            if (this.weapons.Models.Any(w => w.GetType().Name == "NuclearWeapon"))
+            {
+                totalAmount += totalAmount * 0.45;
+            }
 
             return totalAmount;
         }
